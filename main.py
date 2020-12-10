688473319edadaf440ff115e790d0d1f9c7af204
dannyjb123 <dbutchko98@gmail.com>
2020-12-10 09:33:49 -0500

    added working program and files for Ryan's crime alcohol project

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import ryan_geocoder
+import csv
 
 coder = ryan_geocoder.RyanGeocoder()
 
-longs, lats = coder.load_long_lats("C:\\Users\\ladav\\Downloads\\BPD_Part_1_Victim_Based_Crime_Data.csv")
+oldfile = open('Alcohol Data Input.csv', 'r'); # File that stores the lat/lon
+csvreader = csv.reader(oldfile, delimiter=',');
+newfile = open('Alcohol Data Output.csv', 'w', newline=''); # File where we want to save the tract in a new column
+csvwriter = csv.writer(newfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
-
-for i in range(len(longs)):
-    tract = coder.get_tract(longs[i+1], lats[i+1])
-    print(tract)
+for line in csvreader:
+    long=line[10] # longtitude is stored in column 10, where the leftmost column is column 0.
+    lat=line[11] # latitude is stored in column 11, where the leftmost column is column 0.
+    tract = coder.get_tract(long, lat) # push lat/long to site and pull tract
+    #newfile.flush() # refresh the file. This might add time, but it lets you check progress.
+    #line.append(tract) # Add the tract to the end of the row list
+    #csvwriter.writerow(line) # Write the new row to the new document
+    csvwriter.writerow([str(tract)]) #or just write the tract number
+    #print(tract); # print the tract for troubleshooting.
--- ryan_geocoder.py
+++ ryan_geocoder.py
@@ -1,37 +1,18 @@
 import urllib.request
 import json
-import csv
-
 
 class RyanGeocoder():
     def __init__(self):
         pass
 
-    def get_tract(self, long, lat):
+    def get_tract(self, long, lat): # Function pulls tract number from census website given lat/lon.
+        # Create the custom url
         string = 'https://geocoding.geo.census.gov/geocoder/geographies/coordinates?x=' + str(long) + '&y=' + str(lat) + '&vintage=Census2010_Census2010&benchmark=Public_AR_Census2010&format=json&layers=Tract'
-        try:
+        try: # try to request the tract
             response = urllib.request.urlopen(string)
-        except urllib.error.HTTPError:
-            print('Bad request')
-
-        data = json.load(response)
-
-        try:
+            data = json.load(response)
             tract = int(data['result']['geographies']['Census Blocks'][0]['TRACT'])
-        except KeyError:
-            print('Bad Key!!')
+        except: # if an error occurs, set the tract number to 0.
+            tract = 0;
 
         return tract
-
-    def load_long_lats(self, path):
-        longs = []
-        lats = []
-        with open(path, "r") as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            for lines in csv_reader:
-                longs.append(lines[10])
-                lats.append(lines[11])
-
-        return longs, lats
-
-
